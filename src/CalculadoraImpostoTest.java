4e2febfe138f985560e366a2defa068f70484aa1
Giulianobrjf <giuliano.leite2002@gmail.com>
2023-09-15 23:08:45 -0300

    2 testes AC E AL

--- src/CalculadoraImpostoTest.java
+++ src/CalculadoraImpostoTest.java
@@ -1,97 +1,19 @@
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class CalculadoraImpostoTest {
 
     @Test
-    void deveRetornarHomemAbaixoDoPeso() {
-
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(50.0);
-        pessoa.setAltura(1.65);
-        pessoa.setSexo(true);
-        assertEquals("Abaixo do peso Masculino!", pessoa.calcularIMC());
+    public void testAcre() {
+        CalculadoraImposto calculadora = new CalculadoraImposto("AC", 1000.00);
+        double imposto = calculadora.calcularImposto();
+        assertEquals(170.00, imposto, 0.01);
     }
 
     @Test
-    void deveRetornarHomemPesoNormal() {
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(75.0);
-        pessoa.setAltura(1.70);
-        pessoa.setSexo(true);
-        assertEquals("Peso normal Masculino!", pessoa.calcularIMC());
+    public void testAlagoas() {
+        CalculadoraImposto calculadora = new CalculadoraImposto("AL", 1000.00);
+        double imposto = calculadora.calcularImposto();
+        assertEquals(170.00, imposto, 0.01);
     }
-
-    @Test
-    void deveRetornarHomemMarginalmenteAcimaDoPeso() {
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(81.0);
-        pessoa.setAltura(1.75);
-        pessoa.setSexo(true);
-        assertEquals("Marginalmente acima do peso Masculino!", pessoa.calcularIMC());
-    }
-
-    @Test
-    void deveRetornarHomemAcimaDoPesoIdeal() {
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(83.0);
-        pessoa.setAltura(1.70);
-        pessoa.setSexo(true);
-        assertEquals("Acima do peso ideal Masculino!", pessoa.calcularIMC());
-    }
-
-    @Test
-    void deveRetornarHomemObeso() {
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(120.0);
-        pessoa.setAltura(1.90);
-        pessoa.setSexo(true);
-        assertEquals("Obesidade Masculino!", pessoa.calcularIMC());
-    }
-
-    @Test
-    void deveRetornarMulherAbaixoDoPeso() {
-
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(50.0);
-        pessoa.setAltura(1.65);
-        pessoa.setSexo(false);
-        assertEquals("Abaixo do peso Feminino!", pessoa.calcularIMC());
-    }
-
-    @Test
-    void deveRetornarMulherPesoNormal() {
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(70.0);
-        pessoa.setAltura(1.70);
-        pessoa.setSexo(false);
-        assertEquals("Peso normal Feminino!", pessoa.calcularIMC());
-    }
-
-    @Test
-    void deveRetornarMulherMarginalmenteAcimaDoPeso() {
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(81.0);
-        pessoa.setAltura(1.75);
-        pessoa.setSexo(false);
-        assertEquals("Marginalmente acima do peso Feminino!", pessoa.calcularIMC());
-    }
-
-    @Test
-    void deveRetornarMulherAcimaDoPesoIdeal() {
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(83.0);
-        pessoa.setAltura(1.70);
-        pessoa.setSexo(false);
-        assertEquals("Acima do peso ideal Feminino!", pessoa.calcularIMC());
-    }
-
-    @Test
-    void deveRetornarMulherObeso() {
-        Imposto pessoa = new Imposto();
-        pessoa.setPeso(120.0);
-        pessoa.setAltura(1.90);
-        pessoa.setSexo(false);
-        assertEquals("Obesidade Feminino!", pessoa.calcularIMC());
-    }
-
 }
